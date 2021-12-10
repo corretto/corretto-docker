fca699613db178112d0da467ad423e40a40b04f9
Ben Taylor <benty@amazon.com>
2021-12-10 12:54:27 -0800

    Cleanup shellcheck warnings in test-image.sh

--- bin/test-image.sh
+++ bin/test-image.sh
@@ -7,12 +7,12 @@ set -euo pipefail
 #   https://github.com/GoogleContainerTools/container-structure-test
 
 # only linux and osx are supported
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
+if [[ "${OSTYPE}" == "linux-gnu" ]]; then
   cst_ostype="linux"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
+elif [[ "${OSTYPE}" == "darwin"* ]]; then
   cst_ostype="darwin"
 else
-  echo "Unsupported operating system[$OSTYPE]"
+  echo "Unsupported operating system[${OSTYPE}]"
   exit 1
 fi
 mkdir -p build/tool
@@ -23,12 +23,12 @@ chmod +x build/tool/container-structure-test-${cst_ostype}-amd64
 
 # 2. Build the docker image.
 # You can set $1 to a Dockerfile to test interractively
-docker build -f ${1-${DOCKER_FILE}} --tag corretto-docker `dirname ${1-$DOCKER_FILE}}` || \
-docker build -f ${1-${DOCKER_FILE}} --tag corretto-docker `dirname ${1-$DOCKER_FILE}}`
+docker build -f "${1-${DOCKER_FILE}}" --tag corretto-docker "$(dirname "${1-${DOCKER_FILE}}"})" || \
+docker build -f "${1-${DOCKER_FILE}}" --tag corretto-docker "$(dirname "${1-${DOCKER_FILE}}"})"
 
-corretto_version=$(echo ${2-${DOCKER_FILE}} | rev | cut -d'/' -f4 | rev)
-install=$(echo ${3-${DOCKER_FILE}} | rev | cut -d'/' -f3 | rev)
+corretto_version=$(echo "${2-${DOCKER_FILE}}" | rev | cut -d'/' -f4 | rev)
+install=$(echo "${3-${DOCKER_FILE}}" | rev | cut -d'/' -f3 | rev)
 # 3. Test using container-structure-test.
 build/tool/container-structure-test-${cst_ostype}-amd64 test \
     --image corretto-docker \
-    --config test/test-image-corretto${corretto_version}-${install}.yaml
+    --config "test/test-image-corretto${corretto_version}-${install}.yaml"
