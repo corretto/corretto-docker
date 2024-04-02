3d2365e3db44c13a06c19f71aad789e79f67f691
lutkerd <lutkerd@amazon.com>
2024-04-02 17:39:05 +0000

    Make AL2023 images the default for Corretto 22+

--- bin/tag-generator.py
+++ bin/tag-generator.py
@@ -3,44 +3,49 @@ import json
 DEFAULT_ALPINE_VERSION = '3.19'
 ALPINE_VERSIONS = ['3.16', '3.17', '3.18', '3.19']
 
-LTS_VERSIONS = [ "8", "11", "17", "21"]
-FR_VERSIONS = [ "22" ]
 def generate_tags(key, version):
     update = version.split('.')[1] if (key == '8') else version.split('.')[2]
     expanded_version = f"{key}u{update}" if (key == '8') else f"{key}.0.{update}"
 
     al2023_tags = [f"{key}-al2023",  f"{expanded_version}-al2023" ,f"{key}-al2023-jdk"]
-    al2023_generic_tags = [f"{key}-al2023-generic", f"{expanded_version}-al2023-generic", f"{key}-al2023-generic-jdk"]
+    al2023_headless_tags = [f"{key}-al2023-headless",  f"{expanded_version}-al2023-headless"]
+    al2023_headful_tags = [f"{key}-al2023-headful",  f"{expanded_version}-al2023-headful"]
+    # These only apply for Corretto8 which does not have the same modulare packages as 11+
+    al2023_8_tags = [f"{key}-al2023-jre",  f"{expanded_version}-al2023-jre"]
+
     if int(key) <= 21:
         al2_tags = [f"{key}", f"{expanded_version}", f"{expanded_version}-al2", f"{key}-al2-full", f"{key}-al2-jdk", f"{key}-al2-generic", f"{expanded_version}-al2-generic", f"{key}-al2-generic-jdk"]
+        if key == '8':
+            al2_tags.append('latest')
         print("Tags: " + ", ".join(al2_tags) + "")
         print("Architectures: amd64, arm64v8")
         print(f"Directory: {key}/jdk/al2-generic\n")
-    else:
-        al2_tags = []
+
+    # Starting with Corretto 22 AL2 based images are not longer vended and AL2023 will be the
+    # default base OS until the next AL version is GA.
+    if int(key) >= 22:
+        al2023_tags.append(f"{key}")
+        al2023_tags.append(f"{key}-jdk")
+        al2023_headless_tags.append(f"{key}-headless")
+        al2023_headful_tags.append(f"{key}-headful")
+
+    print("Tags: " + ", ".join(al2023_tags) + "")
+    print("Architectures: amd64, arm64v8")
+    print(f"Directory: {key}/jdk/al2023\n")
     if key == '8':
-        al2_tags.append('latest')
-
-
-    if key in LTS_VERSIONS or key in FR_VERSIONS:
-        print("Tags: " + ", ".join(al2023_tags) + "")
+        print("Tags: " + ", ".join(al2023_8_tags))
         print("Architectures: amd64, arm64v8")
         print(f"Directory: {key}/jdk/al2023\n")
-        if key == '8':
-            print("Tags: " + ", ".join([f"{key}-al2023-jre",  f"{expanded_version}-al2023-jre"]))
-            print("Architectures: amd64, arm64v8")
-            print(f"Directory: {key}/jdk/al2023\n")
-        else:
-            print("Tags: " + ", ".join([f"{key}-al2023-headless",  f"{expanded_version}-al2023-headless"]))
-            print("Architectures: amd64, arm64v8")
-            print(f"Directory: {key}/headless/al2023\n")
+    else:
+        print("Tags: " + ", ".join(al2023_headless_tags))
+        print("Architectures: amd64, arm64v8")
+        print(f"Directory: {key}/headless/al2023\n")
 
-            print("Tags: " + ", ".join([f"{key}-al2023-headful",  f"{expanded_version}-al2023-headful"]))
-            print("Architectures: amd64, arm64v8")
-            print(f"Directory: {key}/headful/al2023\n")
+        print("Tags: " + ", ".join(al2023_headful_tags))
+        print("Architectures: amd64, arm64v8")
+        print(f"Directory: {key}/headful/al2023\n")
 
-
-    # For LTS versions with modular AmazonLinux packages we want to tag those images
+    # For LTS versions with modular AmazonLinux 2 packages we want to tag those images
     native_package_modifier="al2-native-"
     if key in ["17"]:
         for image_type in ['headless', 'headful', 'jdk']:
