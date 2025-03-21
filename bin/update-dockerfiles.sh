8088c9d97153f9cd9be7123260680c0530d197ff
lutkerd <lutkerd@amazon.com>
2025-03-21 20:17:11 +0000

    Add ability to change platform version independently

--- bin/update-dockerfiles.sh
+++ bin/update-dockerfiles.sh
@@ -10,28 +10,12 @@ usage() {
     echo "Update the dockerfiles to use the version from versions.json"
 }
 
-update_musl_linux() {
-    CORRETTO_VERSION=$1
-    MAJOR_RELEASE=$2
-
-    ${SED} "s/ARG version=.*/ARG version=${CORRETTO_VERSION}/g" ./${MAJOR_RELEASE}/jdk/alpine/Dockerfile
-    if [ -f ./${MAJOR_RELEASE}/jre/alpine/Dockerfile ]; then
-        ${SED} "s/ARG version=.*/ARG version=${CORRETTO_VERSION}/g" ./${MAJOR_RELEASE}/jre/alpine/Dockerfile
-    fi
-    jdk_version=$(echo ${CORRETTO_VERSION} | cut -d'.' -f1-3)
-
-    ${SED} "s/${MAJOR_RELEASE}\.0\.[0-9]*-alpine/${jdk_version}-alpine/g" README.md
-    if [ -d "./${MAJOR_RELEASE}/slim" ]; then
-        ${SED} "s/ARG version=.*/ARG version=${CORRETTO_VERSION}/g" ./${MAJOR_RELEASE}/slim/alpine/Dockerfile
-    fi
-}
-
 verify_update() {
     MAJOR_RELEASE=$1
     VERSION_STRING=$2
     pushd ${MAJOR_RELEASE}
     DOCKERFILE_COUNT=$(find ./ -name Dockerfile |wc -l)
-    UPDATED_DOCKERFILE_COUNT=$(grep -rl "${VERSION_STRING}" --include Dockerfile |wc -l)
+    UPDATED_DOCKERFILE_COUNT=$(grep -rlE "${VERSION_STRING}" --include Dockerfile |wc -l)
 
     echo "Updated ${UPDATED_DOCKERFILE_COUNT} files for Corretto-${MAJOR_RELEASE}"
     if [[ ${DOCKERFILE_COUNT} == ${UPDATED_DOCKERFILE_COUNT} ]]; then
@@ -43,6 +27,16 @@ verify_update() {
     popd
 }
 
+update_musl_linux() {
+    CORRETTO_VERSION=$1
+    MAJOR_RELEASE=$2
+
+    if [ -d "./${MAJOR_RELEASE}/slim" ]; then
+        ${SED} "s/ARG version=.*/ARG version=${CORRETTO_VERSION}/g" ./${MAJOR_RELEASE}/slim/alpine/Dockerfile
+    fi
+}
+
+
 update_generic_linux() {
     CORRETTO_VERSION=$1
     MAJOR_RELEASE=$2
@@ -53,6 +47,22 @@ update_generic_linux() {
     if [[ -f ./${MAJOR_RELEASE}/jdk/al2-generic/Dockerfile ]]; then
         ${SED} "s/ARG version=.*/ARG version=${jdk_version}.${jdk_build}-${corretto_version}/g" ./${MAJOR_RELEASE}/jdk/al2-generic/Dockerfile
     fi
+
+    ${SED} "s/ARG version=.*/ARG version=${jdk_version}.${jdk_build}-${corretto_version}/g" ./${MAJOR_RELEASE}/jdk/debian/Dockerfile
+
+    if [ -d "./${MAJOR_RELEASE}/slim" ]; then
+        ${SED} "s/ARG version=.*/ARG version=${jdk_version}.${jdk_build}-${corretto_version}/g" ./${MAJOR_RELEASE}/slim/debian/Dockerfile
+        ${SED} "s/ARG version=.*/ARG version=${jdk_version}.${jdk_build}.${corretto_version}/g" ./${MAJOR_RELEASE}/slim/alpine/Dockerfile
+    fi
+}
+
+update_amazon_linux() {
+    CORRETTO_VERSION=$1
+    MAJOR_RELEASE=$2
+
+    jdk_version=$(echo ${CORRETTO_VERSION} | cut -d'.' -f1-3)
+    jdk_build=$(echo ${CORRETTO_VERSION} | cut -d'.' -f4)
+    corretto_version=$(echo ${CORRETTO_VERSION} | cut -d'.' -f5)
     if [[ -f ./${MAJOR_RELEASE}/jdk/al2023/Dockerfile ]]; then
         ${SED} "s/ARG version=.*/ARG version=${jdk_version}.${jdk_build}-${corretto_version}/g" ./${MAJOR_RELEASE}/jdk/al2023/Dockerfile
     fi
@@ -62,9 +72,6 @@ update_generic_linux() {
     if [[ -f ./${MAJOR_RELEASE}/headless/al2023/Dockerfile ]]; then
         ${SED} "s/ARG version=.*/ARG version=${jdk_version}.${jdk_build}-${corretto_version}/g" ./${MAJOR_RELEASE}/headless/al2023/Dockerfile
     fi
-
-
-    ${SED} "s/ARG version=.*/ARG version=${jdk_version}.${jdk_build}-${corretto_version}/g" ./${MAJOR_RELEASE}/jdk/debian/Dockerfile
 
     ADDITIONAL_IMAGES="jdk jre headful headless"
     for IMAGE_TYPE in ${ADDITIONAL_IMAGES}; do
@@ -76,15 +83,8 @@ update_generic_linux() {
         fi
     done
 
-    ${SED} "s/${MAJOR_RELEASE}\.0\.[0-9]*,/${jdk_version},/g" README.md
-    ${SED} "s/${MAJOR_RELEASE}\.0\.[0-9]*-al2/${jdk_version}-al2/g" README.md
     if [[ -f ./${MAJOR_RELEASE}/slim/al2/Dockerfile ]]; then
         ${SED} "s/ARG version=.*/ARG version=${jdk_version}.${jdk_build}-${corretto_version}/g" ./${MAJOR_RELEASE}/slim/al2/Dockerfile
-    fi
-    if [ -d "./${MAJOR_RELEASE}/slim" ]; then
-        ${SED} "s/ARG version=.*/ARG version=${jdk_version}.${jdk_build}-${corretto_version}/g" ./${MAJOR_RELEASE}/slim/debian/Dockerfile
-        ${SED} "s/ARG version=.*/ARG version=${jdk_version}.${jdk_build}.${corretto_version}/g" ./${MAJOR_RELEASE}/slim/alpine/Dockerfile
-        ${SED} "s/${MAJOR_RELEASE}\.0\.[0-9]*-slim,/${jdk_version},/g" README.md
     fi
 }
 
@@ -102,31 +102,51 @@ while [ "$1" != "" ]; do
     shift
 done
 
-CORRETTO_8_GENERIC_LINUX=$(cat versions.json | jq -r '.["8"]' )
-CORRETTO_11_GENERIC_LINUX=$(cat versions.json | jq -r '.["11"]' )
-CORRETTO_17_GENERIC_LINUX=$(cat versions.json | jq -r '.["17"]' )
-CORRETTO_21_GENERIC_LINUX=$(cat versions.json | jq -r '.["21"]' )
-CORRETTO_23_GENERIC_LINUX=$(cat versions.json | jq -r '.["23"]' )
-CORRETTO_24_GENERIC_LINUX=$(cat versions.json | jq -r '.["24"]' )
+VERSIONS=$(jq -r 'keys |@sh' versions.json|tr -d \')
+PLATFORMS=("GENERIC_LINUX" "ALPINE" "AMAZON_LINUX")
+for version in ${VERSIONS}; do
+    # If the version entry is just a string then populate all of the PLATFORMS with that value
+    if [[ "$(cat versions.json |jq -r ".[\"${version}\"]|type")" == "string" ]]; then
+        for platform in "${PLATFORMS[@]}"; do
+            eval CORRETTO_${version}_${platform}="$(cat versions.json |jq -r ".[\"${version}\"]")"
+        done
+    else
+        # If the version entry is not a string then it must be and map containing all of the PLATFORMS
+        for platform in "${PLATFORMS[@]}"; do
+            eval CORRETTO_${version}_${platform}="$(cat versions.json |jq -r ".[\"${version}\"][\"${platform}\"]")"
+        done
+    fi
+done
+
 
 if [ ! -z "${CORRETTO_11_GENERIC_LINUX}" ]; then
     update_generic_linux ${CORRETTO_11_GENERIC_LINUX} 11
+    update_amazon_linux ${CORRETTO_11_AMAZON_LINUX} 11
+    update_musl_linux ${CORRETTO_11_ALPINE} 11
 fi
 
 if [ ! -z "${CORRETTO_17_GENERIC_LINUX}" ]; then
     update_generic_linux ${CORRETTO_17_GENERIC_LINUX} 17
+    update_amazon_linux ${CORRETTO_17_AMAZON_LINUX} 17
+    update_musl_linux ${CORRETTO_17_ALPINE} 17
 fi
 
 if [ ! -z "${CORRETTO_21_GENERIC_LINUX}" ]; then
     update_generic_linux ${CORRETTO_21_GENERIC_LINUX} 21
+    update_amazon_linux ${CORRETTO_21_AMAZON_LINUX} 21
+    update_musl_linux ${CORRETTO_21_ALPINE} 21
 fi
 
 if [ ! -z "${CORRETTO_23_GENERIC_LINUX}" ]; then
     update_generic_linux ${CORRETTO_23_GENERIC_LINUX} 23
+    update_amazon_linux ${CORRETTO_23_AMAZON_LINUX} 23
+    update_musl_linux ${CORRETTO_23_ALPINE} 23
 fi
 
 if [ ! -z "${CORRETTO_24_GENERIC_LINUX}" ]; then
     update_generic_linux ${CORRETTO_24_GENERIC_LINUX} 24
+    update_amazon_linux ${CORRETTO_24_AMAZON_LINUX} 24
+    update_musl_linux ${CORRETTO_24_ALPINE} 24
 fi
 
 if [ ! -z "${CORRETTO_8_GENERIC_LINUX}" ]; then
@@ -134,15 +154,18 @@ if [ ! -z "${CORRETTO_8_GENERIC_LINUX}" ]; then
     jdk_build=$(echo ${CORRETTO_8_GENERIC_LINUX} | cut -d'.' -f3)
     corretto_version=$(echo ${CORRETTO_8_GENERIC_LINUX} | cut -d'.' -f4)
     ${SED} "s/ARG version=.*/ARG version=1.8.0_${jdk_version}.b${jdk_build}-${corretto_version}/g" ./8/jdk/al2-generic/Dockerfile
+    ${SED} "s/ARG version=.*/ARG version=8.${jdk_version}.${jdk_build}-${corretto_version}/g" ./8/jdk/debian/Dockerfile
+fi
+
+if [ ! -z "${CORRETTO_8_AMAZON_LINUX}" ]; then
+    jdk_version=$(echo ${CORRETTO_8_AMAZON_LINUX} | cut -d'.' -f2)
+    jdk_build=$(echo ${CORRETTO_8_AMAZON_LINUX} | cut -d'.' -f3)
+    corretto_version=$(echo ${CORRETTO_8_AMAZON_LINUX} | cut -d'.' -f4)
+
     ${SED} "s/ARG version=.*/ARG version=1.8.0_${jdk_version}.b${jdk_build}-${corretto_version}/g" ./8/jdk/al2/Dockerfile
     ${SED} "s/ARG version=.*/ARG version=1.8.0_${jdk_version}.b${jdk_build}-${corretto_version}/g" ./8/jre/al2/Dockerfile
     ${SED} "s/ARG version=.*/ARG version=1.8.0_${jdk_version}.b${jdk_build}-${corretto_version}/g" ./8/jre/al2023/Dockerfile
     ${SED} "s/ARG version=.*/ARG version=1.8.0_${jdk_version}.b${jdk_build}-${corretto_version}/g" ./8/jdk/al2023/Dockerfile
-    ${SED} "s/ARG version=.*/ARG version=8.${jdk_version}.${jdk_build}-${corretto_version}/g" ./8/jdk/debian/Dockerfile
-
-
-    ${SED} "s/8u[0-9]*,/8u${jdk_version},/g" README.md
-    ${SED} "s/8u[0-9]*-al2/8u${jdk_version}-al2/g" README.md
 fi
 
 find . -name "*.bkp" | xargs rm -rf
@@ -151,8 +174,8 @@ python3 bin/apply-template.py
 
 # 8 is special and doesn't have a consistent version string, so we just use the update version.
 verify_update 8 ${jdk_version}
-verify_update 11 ${CORRETTO_11_GENERIC_LINUX}
-verify_update 17 ${CORRETTO_17_GENERIC_LINUX}
-verify_update 21 ${CORRETTO_21_GENERIC_LINUX}
-verify_update 23 ${CORRETTO_23_GENERIC_LINUX}
-verify_update 24 ${CORRETTO_24_GENERIC_LINUX}
+verify_update 11 "${CORRETTO_11_GENERIC_LINUX}|${CORRETTO_11_AMAZON_LINUX}|${CORRETTO_11_ALPINE}"
+verify_update 17 "${CORRETTO_17_GENERIC_LINUX}|${CORRETTO_17_AMAZON_LINUX}|${CORRETTO_17_ALPINE}"
+verify_update 21 "${CORRETTO_21_GENERIC_LINUX}|${CORRETTO_21_AMAZON_LINUX}|${CORRETTO_21_ALPINE}"
+verify_update 23 "${CORRETTO_23_GENERIC_LINUX}|${CORRETTO_23_AMAZON_LINUX}|${CORRETTO_23_ALPINE}"
+verify_update 24 "${CORRETTO_24_GENERIC_LINUX}|${CORRETTO_24_AMAZON_LINUX}|${CORRETTO_24_ALPINE}"
