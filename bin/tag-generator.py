f4da4b768fd7e5458fde84cffea88f0d956cf7dd
Satyen Subramaniam <satyenme@amazon.com>
2026-01-20 15:25:47 -0800

    Adding alpine 3.23

--- bin/tag-generator.py
+++ bin/tag-generator.py
@@ -1,7 +1,7 @@
 import json
 
-DEFAULT_ALPINE_VERSION = '3.22'
-ALPINE_VERSIONS = ['3.20', '3.21', '3.22']
+DEFAULT_ALPINE_VERSION = '3.23'
+ALPINE_VERSIONS = ['3.20', '3.21', '3.22', '3.23']
 
 def generate_tags(key, version):
     if type(version) is str:
